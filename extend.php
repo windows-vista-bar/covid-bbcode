4f1d72270433ec23809ea8e14c84a3d625ca987a
JC-ProPlus <guo121guo@gmail.com>
2020-04-14 18:39:12 +0800

    Mobile view added

--- extend.php
+++ extend.php
@@ -6,6 +6,10 @@ return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
                 '[covid-dash]',
-                '<iframe name=covid-dash frameborder="0" scrolling="auto" width="100%" height=600 οnlοad="document.all[\'covid-dash\'].style.width=myframe.document.body.scrollWidth" src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe>',
+                '<iframe name=covid-dash frameborder="0" scrolling="auto" width="100%" height=600 οnlοad="document.all[\'covid-dash\'].style.width=myframe.document.body.scrollWidth" src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe>'
+            );
+            $config->BBCodes->addCustom(
+                '[covid-mobile]',
+                '<iframe name=covid-mobile frameborder="0" scrolling="auto" width="100%" height=600 οnlοad="document.all[\'covid-dash\'].style.width=myframe.document.body.scrollWidth" src="https://www.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61"></iframe>'
             );
         });
