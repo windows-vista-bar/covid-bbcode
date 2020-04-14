bec1c0c27a103bf97425ad12f9362b9855852872
2006bt <m2006bt@outlook.com>
2020-04-14 17:06:41 +0800

    Update extend.php
    It can be used.

--- extend.php
+++ extend.php
@@ -5,7 +5,7 @@ use s9e\TextFormatter\Configurator;
 return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
-                '[covid-dash{TEXT}]',
-                '<iframe src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe>',
+                '[covid-dash]',
+                '<iframe name=covid-dash frameborder="0" scrolling="auto" width="100%" height=600 οnlοad="document.all[\'covid-dash\'].style.width=myframe.document.body.scrollWidth" src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe>',
             );
         });
