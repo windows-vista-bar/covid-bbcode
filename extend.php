16a2db806008691b95430f50654ce07c8328d0d6
JC-ProPlus <guo121guo@gmail.com>
2020-04-14 16:05:56 +0800

    Bugfix&Update .gitignore

--- extend.php
+++ extend.php
@@ -5,7 +5,7 @@ use s9e\TextFormatter\Configurator;
 return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
-                '[covid-dash]',
+                '[covid-dash{TEXT}]',
                 '<iframe src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe>',
             );
         });
